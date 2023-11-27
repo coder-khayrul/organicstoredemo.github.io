0d63d2577107229e92bd8f64e0224f655b350a49
khayrul200822 <khayrulislam200822@gmail.com>
2023-11-27 23:45:20 +0600

    update style

--- js/accordion_menu.js
+++ js/accordion_menu.js
@@ -1,6 +1,28 @@
 //Create Function
 $(function () {
+    let toggleIcons = document.querySelectorAll(".fa-chevron-down");
+    let submenus = document.querySelectorAll(".dropdownlink");
+    
+    let submenu
+    let isMenuClicked = false;
+    for(let i = 0; i < 14; i++){
+       submenu = submenus[i];
+       
+    }
+    submenu.addEventListener("click", () => {
+      if (isMenuClicked) {
+        toggleIcons.forEach(icon => icon.classList.add("active_menu_icon"));
+        console.log("clicked");
+      } else {
+        toggleIcons.forEach(icon => icon.classList.remove("active_menu_icon"));
+        console.log("removed");
+      }
+    
+      isMenuClicked = !isMenuClicked;
+    });
 
+    
+    })
     var Accordion = function (el, multiple) {
         this.el = el || {};
         //More Than One Menu Open
@@ -25,8 +47,8 @@ $(function () {
         $this.parent().toggleClass('open');
 
         // if (!e.data.multiple) {
-          //Show Only one Menu at the same time
-            // $el.find('.submenuItems').not($next).slideUp().parent().removeClass('open');
+        //Show Only one Menu at the same time
+        // $el.find('.submenuItems').not($next).slideUp().parent().removeClass('open');
         // }
 
     };
@@ -34,5 +56,5 @@ $(function () {
 
     //Set Object
     var accordion = new Accordion($('.accordion-menu'), false);
-});
+
 
